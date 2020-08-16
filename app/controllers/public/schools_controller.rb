7641ea86840a05fe8414654b715b73497ba0559c
siragasnow <siraga613@gmail.com>
2020-08-16 23:31:21 +0900

    deploy setting 4

--- app/controllers/public/schools_controller.rb
+++ app/controllers/public/schools_controller.rb
@@ -1,25 +1,16 @@
 class Public::SchoolsController < ApplicationController
   def index
-    @school = current_school
   end
 
   def show
-    @school = School.find(params[:id])
-    @menus = @school.menus
   end
 
   def edit
-    @school = School.find(params[:id])
   end
 
   def update
-    @school = School.find(params[:id])
-    @school.update(school_params)
-    redirect_to public_school_path
   end
 
-  private
-  def school_params
-    params.require(:school).permit(:name, :email, :postcode, :street_address, :phone_number)
+  def destroy
   end
 end
