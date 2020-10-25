88517809f2fbe1c355c7e6550c0520a593fea283
siragasnow <siraga613@gmail.com>
2020-10-25 14:23:19 +0900

    [add]プレビュー機能

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,3 +41,19 @@ $(function(){
 			$("#new-menu-icon").css('display', 'none').delay(300).fadeIn('fast');
 	});
 });
+
+$(function () {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('.img_prev').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $('.img_field').change(function () {
+    readURL(this);
+  });
+});
